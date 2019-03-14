e73e8ca012a5dcdabc7f7e5a4080a85456851434
aman-maharshi <amanm4harshi@gmail.com>
2019-03-14 12:12:59 +0530

    added a static trash icon to first li

--- c5-dom/script.js
+++ c5-dom/script.js
@@ -1,10 +1,14 @@
 
 let hotItems = document.querySelectorAll('.hot');
-
 for (let i = 0; i < hotItems.length; i++) {
     // console.log(hotItems[i].textContent);
     if (i%2 == 0) {
         hotItems[i].className = "cool";
     }
-    
 }
+
+// adding a trash box to the first li
+let i = document.getElementsByTagName('i');
+
+i[1].classList.add('fas');
+i[1].classList.add('fa-trash-alt')
